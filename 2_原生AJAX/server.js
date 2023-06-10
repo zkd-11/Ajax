a46924b3e4e62a8c237e2ce2414c161b56b56d47
zkd-11 <1137756821@qq.com>
2023-06-10 16:00:43 +0800

    up3

--- 2_原生AJAX/server.js
+++ 2_原生AJAX/server.js
@@ -25,6 +25,23 @@ app.post('/server', (req, res) => {
     res.send('hello AJax_post');
 })
 
+app.all('/json-server', (req, res) => {
+    //设置响应头， 允许跨域
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+     res.setHeader('Access-Control-Allow-Headers', '*');
+    // 设置响应
+
+    //响应一个数据
+    const data ={
+        name : 'HanJue'
+    };
+
+    // 对 对象进行 字符串转化
+    let str = JSON.stringify(data);
+
+    res.send(str);
+})
 
 // 4. 监听端口启动服务
 app.listen(8000, ()=>{
